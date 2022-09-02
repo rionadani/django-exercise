3744d87fe2b569f2a8e08810ff77e8217f031567
rionadani <rionadani@gmail.com>
2022-09-02 09:50:08 +0700

    Fix

--- rionadani/asgi.py
+++ rionadani/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for FIRST_PROJECT project.
+ASGI config for rionadani project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FIRST_PROJECT.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rionadani.settings')
 
 application = get_asgi_application()
--- rionadani/wsgi.py
+++ rionadani/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for FIRST_PROJECT project.
+WSGI config for rionadani project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FIRST_PROJECT.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rionadani.settings')
 
 application = get_wsgi_application()
